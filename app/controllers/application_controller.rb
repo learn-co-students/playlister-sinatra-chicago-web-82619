6559cdbff9f3b3466014aec88bc9c2b7de941ae2
joseph-gansukh <g.sukhbat@gmail.com>
2019-09-19 17:06:25 -0500

    Added update feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  configure do 
+    set :method_override, true
+  end
+
   get '/' do
     erb :index
   end
