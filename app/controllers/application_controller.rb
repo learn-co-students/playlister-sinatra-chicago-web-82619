260e8d4396867ea4fb6bda411cd78bd79c42f5a9
justrmarks <blackbirdwanderer@gmail.com>
2019-09-19 17:57:55 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
+
+
 class ApplicationController < Sinatra::Base
+  require 'rack-flash'
+
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  use Rack::Flash
+  enable :sessions
+
 
   get '/' do
     erb :index
--- config.ru
+++ config.ru
@@ -3,7 +3,12 @@ require './config/environment'
 begin
   fi_check_migration
 
+
   use Rack::MethodOverride
+  # use Rack::Flash
+  use ArtistsController
+  use SongsController
+  use GenresController
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
